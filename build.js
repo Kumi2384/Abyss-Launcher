c51264fb1d89579229a03728cf2911d917bcf476
Kumi2384 <benhassanlucas@gmail.com>
2025-06-24 18:36:41 +0200

    test

--- build.js
+++ build.js
@@ -149,6 +149,7 @@ class Index {
 const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
+  const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
